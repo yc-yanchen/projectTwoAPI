24d151d1025132ae205cd30d1f6f825eb1497844
YanChen <yc.yanchen@hotmail.com>
2022-11-11 09:47:43 -0500

    Implemented JS to display questions to the page. Added more comments.

--- game.js
+++ game.js
@@ -1,31 +1,45 @@
+// Create namespace
 const triviaApp = {};
 
-triviaApp.url = new URL("https://the-trivia-api.com/api/questions");
-
-triviaApp.init = () => {
+// Create initialization function
+triviaApp.init = function () {
     getQuestion();
 };
 
+// Create an array to store the parsed json data
+triviaApp.questionArray = [];
+
+// variable to store the api url endpoint
+triviaApp.url = new URL("https://the-trivia-api.com/api/questions");
+
+// Search parameters for the url endpoint
 triviaApp.url.search = new URLSearchParams({
-    amount: 10,
+    // To request for the number of questions
+    limit: 10,
+    // To determine the difficulty level of the questions
     difficulty: "easy",
 });
 
-triviaApp.questionArray = [];
-
+// Async function to fetch for the api data
 async function getQuestion() {
     const myObject = await fetch(triviaApp.url);
     const myQuestion = await myObject.json();
     triviaApp.questionArray.push(myQuestion);
-    console.log(triviaApp.questionArray[0]);
+    console.log(triviaApp.questionArray);
+
+    // Call the displayQuestion function
+    triviaApp.displayQuestion();
 }
 
+// Function to display the questions inside the questionArray
 triviaApp.displayQuestion = () => {
+    // Create an h2 object in the document
     const questionElement = document.createElement("h2");
-    // questionElement.innerHTML = triviaApp.questionArray[0][0].question;
-    // console.log(triviaApp.questionArray);
+    // Adding text content to the h2
+    questionElement.innerHTML = triviaApp.questionArray[0][0].question;
+    // Appending the h2 object to the page of the html
+    document.querySelector(".content").append(questionElement);
 };
 
-triviaApp.displayQuestion();
-
+// Call the initialization function
 triviaApp.init();
