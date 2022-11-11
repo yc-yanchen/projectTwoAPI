51f0aba939387a1064fceb2fa9cfcd4ea8aff6d8
ana.vahcic <ana.vahcic@gmail.com>
2022-11-11 14:44:31 +0100

    added JavaScript to retrieve API data and parse json

--- game.js
+++ game.js
@@ -1,11 +1,25 @@
 const triviaApp = {}
 
-triviaApp.url = new URL("https://opentdb.com/api.php");
+triviaApp.url = new URL("https://the-trivia-api.com/api/questions");
 
-triviaApp.init = () {
+triviaApp.init = () => {
+    getQuestion()
+};
 
-}
-// Code comes here
+triviaApp.url.search = new URLSearchParams({
+    amount: 10,
+    difficulty: "easy"
+});
+
+triviaApp.questionArray = []
+
+async function getQuestion() {
+    const myObject = await fetch(triviaApp.url)
+    const myQuestion = await myObject.json()
+    triviaApp.questionArray.push(myQuestion)
+    console.log(triviaApp.questionArray)
+};
+
 
 
 
