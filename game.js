63a4e4105e6fa7cc03b0335d6589148e9a6cfae2
YanChen <yc.yanchen@hotmail.com>
2022-11-11 09:20:44 -0500

    Start implementation of js function to display question

--- game.js
+++ game.js
@@ -1,28 +1,31 @@
-const triviaApp = {}
+const triviaApp = {};
 
 triviaApp.url = new URL("https://the-trivia-api.com/api/questions");
 
 triviaApp.init = () => {
-    getQuestion()
+    getQuestion();
 };
 
 triviaApp.url.search = new URLSearchParams({
     amount: 10,
-    difficulty: "easy"
+    difficulty: "easy",
 });
 
-triviaApp.questionArray = []
+triviaApp.questionArray = [];
 
 async function getQuestion() {
-    const myObject = await fetch(triviaApp.url)
-    const myQuestion = await myObject.json()
-    triviaApp.questionArray.push(myQuestion)
-    console.log(triviaApp.questionArray)
+    const myObject = await fetch(triviaApp.url);
+    const myQuestion = await myObject.json();
+    triviaApp.questionArray.push(myQuestion);
+    console.log(triviaApp.questionArray[0]);
+}
+
+triviaApp.displayQuestion = () => {
+    const questionElement = document.createElement("h2");
+    // questionElement.innerHTML = triviaApp.questionArray[0][0].question;
+    // console.log(triviaApp.questionArray);
 };
 
-
-
-
-
+triviaApp.displayQuestion();
 
 triviaApp.init();
